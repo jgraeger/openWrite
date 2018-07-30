9cd884dedc10339d58ee27c1ac1ac9dac1d1e661
Johannes Gräger <j.graeger@campus.tu-berlin.de>
2018-07-30 22:34:07 +0200

    Initial Commit

---
+++ openWrite/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  openWrite
+//
+//  Created by Johannes Gräger on 30.07.18.
+//  Copyright © 2018 Johannes Gräger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ openWrite/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  openWrite
+//
+//  Created by Johannes Gräger on 30.07.18.
+//  Copyright © 2018 Johannes Gräger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
